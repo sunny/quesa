c69614ff01adcd08fe3142b503dee41773bfe759
Sunny Ripert <sunny@sunfox.org>
2014-01-22 19:46:30 +0100

    Pas de cabaret en janvier

--- lib/config.php
+++ lib/config.php
@@ -11,10 +11,6 @@ setlocale (LC_ALL, "fr_FR");
 
 // Data
 $EVENTS = array(
-  new Event("2013-10-24 21:00", "", "https://www.facebook.com/events/349197361893739/"),
-  new Event("2013-11-28 21:00", "", "http://bit.ly/Quesapelorio-Cabaret-novembre-2013"),
-  new Event("2013-12-18 21:00", "", ""), # http://bit.ly/Quesapelorio-Cabaret-decembre-2013
-  new Event("2014-01-23 21:00", "", ""), # http://bit.ly/Quesapelorio-Cabaret-janvier-2014
   new Event("2014-02-27 21:00", "", ""), # http://bit.ly/Quesapelorio-Cabaret-fevrier-2014
   new Event("2014-03-27 21:00", "", ""), # http://bit.ly/Quesapelorio-Cabaret-mars-2013
   new Event("2014-04-24 21:00", "", ""), # http://bit.ly/Quesapelorio-Cabaret-avril-2014
