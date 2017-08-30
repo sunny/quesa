89dc0d375c3d06793f7a23c3b0a1c6d38cd8b6aa
Sunny Ripert <sunny@sunfox.org>
2017-08-30 20:03:38 +0200

    Update events in data

--- data.php
+++ data.php
@@ -6,9 +6,5 @@ $EVENTS = array(
   #           "Nous avons le plaisir venir à Hong Kong pour vous proposer 2 représentations exceptionnelles les 16 et 17 octobre 2015.",
   #           "https://www.facebook.com/events/519773924844064/",
   #           "hk"),
-  # new Event("2015-10-17 21:00",
-  #           "Nous avons le plaisir venir à Hong Kong pour vous proposer 2 représentations exceptionnelles les 16 et 17 octobre 2015.",
-  #           "https://www.facebook.com/events/519773924844064/",
-  #           "hk"),
-  new Event("2016-12-04 18:30", "", "https://www.facebook.com/events/1827878467447075/", "les-ecuries"),
+  new Event("2017-09-03 18:30", "", "https://www.facebook.com/events/357812764652490/", "les-ecuries"),
 );
